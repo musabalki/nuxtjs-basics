0d914c7b56f1bbf688e00219b3e39005b36dacf8
musabalki <musabalkimb@gmail.com>
2023-03-17 23:55:58 +0300

    Create nuxtjs

---
+++ nuxt/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    BUTTON
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        INDEX
+        test
+    </div>
+</template>
+<script setup>
+
+</script>
