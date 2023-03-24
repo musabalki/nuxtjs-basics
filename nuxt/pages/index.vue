a996306d123ce13c7a2821fdd71ad3a394225dcb
musabalki <musabalkimb@gmail.com>
2023-03-24 23:54:54 +0300

    Add usefetch

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -8,16 +8,26 @@
         <ProfileHeader/>
         <!-- <img src="@/assets/" alt=""> -->
         <button class="px-4 py-2 rounded bg-cyan-500 text-white" @click="setCount(2)">Count</button>
-        {{ count }}
+
+        <div v-if="pending==false">
+            <div v-for="d in data" :key="d.id">
+                {{ d.title }}
+            </div>
+        </div>
+        <div v-else>
+            loading
+        </div>
     </div>
 </template>
 
 <script setup >
+//const {data} = await useFetch("https://jsonplaceholder.typicode.com/posts")
+const {data,pending} = await useLazyFetch("https://jsonplaceholder.typicode.com/posts")
 //import {useUtils} from '../composables/useUtils'
-definePageMeta({
+/*definePageMeta({
     middleware:"auth"
 })
 const {count,getCount,setCount} = useUtils();
 const res = await $fetch('/api/hello')
-console.log(res)
+console.log(res)*/
 </script>
