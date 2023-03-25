694eb7591e15590d5c30fa38136de6e14b5f2056
musabalki <musabalkimb@gmail.com>
2023-03-25 23:50:25 +0300

    Add seo, usehead

--- nuxt/pages/custom.vue
+++ nuxt/pages/custom.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
+        <Head>
+            <Title>
+                TITLE
+            </Title>
+        </Head>
         user page
     </div>
 </template>
 
 <script setup>
 definePageMeta({
-    layout:"user"
+    layout:"user",
+    script:[
+        {src:"",body:true}
+    ]
 })
+/*
+useHead({
+    title:"CUSTOM",
+    meta:[
+        {name:'description',content:'my'}
+    ]
+})*/
 </script>
