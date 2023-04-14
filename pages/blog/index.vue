3f726e892564b93d8b4b2dda134aa8448875a841
musabalki <musabalkimb@gmail.com>
2023-04-15 00:50:21 +0300

    Add jsonplaceholder

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -18,21 +18,29 @@
             <div class="container">
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10">
                     
-                    <div v-for="item in data" :key="item.id" class="box-s rounded overflow-hidden min-h-[200px] bg-white border ">
-                        <div class="flex flex-col p-6 ">
-                            <a href="detail.html" class="my-2 text-justify text-xl	md:text-2xl font-bold">
-                                {{ item.title }}
-                            </a>
-                            <p class="text-justify text-sm">
-                                {{ item.body }}
-                            </p>
+                    <div v-for="item in posts" :key="item.id" class="box-s flex flex-col justify-between items-start rounded overflow-hidden min-h-[200px] bg-white border ">
+                        <div class="flex flex-col justify-between  p-6 h-full">
+                            <div class="flex flex-col">
+                                <a href="detail.html" class="my-2 text-justify text-xl	md:text-2xl font-bold">
+                                    {{ item.title }}
+                                </a>
+                                <p class="text-justify text-sm flex-1">
+                                    {{ item.body }}
+                                </p>
+                            </div>
+                            <NuxtLink :to="'/blog/'+item.id" class="px-4 py-2 bg-indigo-400 rounded mt-4 inline text-center text-white" >
+                                Detail
+                            </NuxtLink>
                         </div>
+                        
                     </div>
-    
     
                 </div>
                 <div class="text-center flex justify-center my-6">
-                    <p class="bg-gray-800 cursor-pointer     px-6 py-1 rounded text-white">Load More</p>
+                    <p @click="loadMore" class="bg-gray-800 cursor-pointer     px-6 py-1 rounded text-white">
+                        <span v-if="!loading">Load More</span>
+                        <span v-else>Loading...</span>
+                    </p>
                 </div>
                 
             </div>  
@@ -42,5 +50,26 @@
 </template>
 
 <script setup>
-const {data} = await useFetch('https://jsonplaceholder.typicode.com/posts')
+import {onMounted} from "vue"
+
+const limit = ref(3)
+const offset = ref(10)
+const page = ref(1)
+const posts = ref([])
+const loading = ref(false)
+
+
+const getPosts = async () =>{
+    loading.value = true
+    let {data} = await useFetch(`https://jsonplaceholder.typicode.com/posts?_page=${page.value}&_limit=${limit.value}`)
+     posts.value = [...posts.value,...data.value]
+    loading.value = false
+}
+
+getPosts()
+
+const loadMore = () =>{
+       page.value=page.value+1
+       getPosts();
+}
 </script>
