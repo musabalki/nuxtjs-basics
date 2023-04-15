63610464a8857c8f06e667a3fe87eb9e89dd789b
musabalki <musabalkimb@gmail.com>
2023-04-16 01:48:30 +0300

    Add detail page

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        {{ $route.params.id }}
+    <div class="container mx-auto rounded ">
+        <div class="bg-gray-50 p-5 text-center">
+            <h1 class="font-bold text-2xl pb-5">{{data.title}}</h1>
+            <p class="pb-5"> {{data.body}}</p>
+        </div>
     </div>
 </template>
+
+<script setup>
+const route = useRoute();
+let {data} = await useFetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+console.log(data.value)
+</script>
