317697cf05ee62d2e9284d888a07608361a9395e
musabalki <musabalkimb@gmail.com>
2023-04-17 02:35:33 +0300

    Add detail page prev,next button

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:true,
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+    apiSecret: '123',
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      apiBase: 'https://jsonplaceholder.typicode.com'
+    }
+  },
   alias:{
     assets:"/<rootDir></assets",
   },
--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -3,12 +3,28 @@
         <div class="bg-gray-50 p-5 text-center">
             <h1 class="font-bold text-2xl pb-5">{{data.title}}</h1>
             <p class="pb-5"> {{data.body}}</p>
+            <div class="flex justify-between">
+                <button @click="change('prev')" class="bg-green-500 text-white px-4 py-2 rounded">Prev</button>
+                <button @click="change('next')" class="bg-green-500 text-white px-4 py-2 rounded">Next</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 const route = useRoute();
-let {data} = await useFetch(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
-console.log(data.value)
+const router = useRouter();
+const api = useRuntimeConfig();
+
+
+let {data} = await useFetch(`${api.public.apiBase}/posts/${route.params.id}`)
+
+const change = (p) =>{
+    if(p=='prev' && route.params.id>0){
+        router.push(`/blog/${parseInt(route.params.id)-1}`)
+    }else if(p=='next' && route.params.id>0){
+        router.push(`/blog/${parseInt(route.params.id) +1}`)
+    }
+}
+
 </script>
